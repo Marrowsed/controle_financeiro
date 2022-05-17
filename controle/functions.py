6d3dcf3b20799558b5fab2675ac8fe11274abc86
Marrowsed <marcus.ah@gmail.com>
2022-05-16 22:06:59 -0300

    new functions for view

--- controle/functions.py
+++ controle/functions.py
@@ -22,6 +22,36 @@ def sum_parcelas(parcelas):
     return soma
 
 
+def sum_parcela_final(lista):
+    soma = 0
+    for l in lista:
+        parcela = int(l.parcela.split('/')[-1])
+        if parcela > 1:
+            soma_parcela = l.valor / parcela
+            soma += soma_parcela
+    return soma
+
+def sum_saida_geral(saida):
+    """
+    Soma das Saidas
+    """
+    soma = 0
+    for s in saida:
+        if s.tipo != "Compra Parcelada":
+            soma += s.valor
+    return soma
+
+
+def sum_saida(parcelas, lista, saida):
+    """
+    Soma dos pagamentos pelo filtro Saída + Parcelados
+    """
+    parcela = sum_parcelas(parcelas)
+    parcela_final = sum_parcela_final(lista)
+    saida = sum_saida_geral(saida)
+    return parcela + parcela_final + saida
+
+
 def sum_faturas(faturas):
     """
     Soma dos pagamentos pelo filtro
@@ -65,11 +95,25 @@ def is_valid_entrada(entrada, conta):
     return conta.valor
 
 
-def filter_by_model_date_conta(model, month, year,field):
+def filter_by_model_conta(model, field):
+    """
+    Filtro por Model e conta
+    """
+    return model.objects.filter(conta=field)
+
+
+def filter_by_model_date_conta(model, month, year, field):
     """
     Fitro por Model para data/mês e conta
     """
     return model.objects.filter(data__month=month, data__year=year, conta=field)
+
+
+def replace_month_year(data, month, year):
+    """
+    Encapsulamento da função replace
+    """
+    return data.replace(month=int(month), year=int(year))
 
 
 def check_final_date(data, parcela):
@@ -77,3 +121,32 @@ def check_final_date(data, parcela):
     Retorna a data da parcela final
     """
     return data + timedelta(seconds=parcela * 30 * 24 * 60 * 60)
+
+
+def check_is_parcelado(model, field, year, month):
+    """
+    Retorna uma lista que será exibida as compras parceladas !
+    """
+    compra_saida = filter_by_model_conta(model, field)
+    list_saida = []
+    for c in compra_saida:
+        if get_date_iso_format(year, month) >= c.data:
+            list_saida.append(c)
+            data_final = check_final_date(c.data, c.parcela)
+            parcela = (int(month) - c.data.month)
+            if data_final > replace_month_year(get_date_iso_format(year, month), month, year):
+                if parcela > 0:
+                    c.parcela = f"{parcela}/{c.parcela}"
+                else:
+                    c.parcela = f"{parcela + 12}/{c.parcela}"
+                c.data = replace_month_year(c.data, month, year)
+            elif data_final < replace_month_year(get_date_iso_format(year, month), month, year):
+                list_saida.remove(c)
+    return list_saida
+
+
+def get_date_iso_format(year, month):
+    """
+    Retorna uma data em String year/month/01
+    """
+    return datetime.fromisoformat(f"{year}-{month}-01").date()
