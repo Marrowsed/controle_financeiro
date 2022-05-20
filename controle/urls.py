f197765ce22d16a5720c4cedf006d0a0079428da
Marrowsed <marcus.ah@gmail.com>
2022-05-20 11:23:09 -0300

    new URLs

--- controle/urls.py
+++ controle/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path('conta/<str:pk>/edita', edita_conta, name="edita_conta"),
     path('conta/<str:pk>/deleta', deleta_conta, name="deleta_conta"),
     path('conta/<str:pk>/entrada', add_entrada, name="adiciona_entrada"),
+    path('conta/<str:pk>/entrada/<str:id>', show_entrada, name="mostra_entrada"),
+    path('conta/<str:pk>/entrada/<str:id>/deleta', delete_entrada, name="deleta_entrada"),
     path('conta/<str:pk>/saida', add_saida, name="adiciona_saida"),
+    path('conta/<str:pk>/saida/<str:id>', show_saida, name="mostra_saida"),
+    path('conta/<str:pk>/saida/<str:id>/deleta', delete_saida, name="deleta_saida"),
+
 
 
 ]
